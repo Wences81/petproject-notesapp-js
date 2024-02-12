12fad54dec25f8476a6cb89912cf0cdabe5e599e
Wences81 <zhylinpochta@gmail.com>
2024-02-12 17:36:46 +0100

    12/02/2024

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-const mainEl = document.querySelector('.main');
+const notesEl = document.querySelector('.notes');
+const addBtn = document.querySelector('.note-add');
+
+function createNote(title, text) {
+    const noteEl = document.createElement('div');
+    noteEl.classList.add('note');
+    noteEl.innerHTML = `
+    <div class='note-header'>
+      <p>${title}</p>
+      <div class='note-actions'>
+        <button class='note-edit'><i class="fa-solid fa-pen-to-square"></i></button>
+        <button class='note-delete'><i class="fa-solid fa-trash"></i></button>
+      </div>
+    </div>
+    <p id='note-description'>${text}</p>
+    `
+
+    return noteEl;
+}
+
+addBtn.addEventListener('click', (e) => {
+    const el = createNote('Title', 'Text');
+    notesEl.appendChild(el);
+})
