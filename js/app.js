3418eb09868a86976c24a3b2a74f22002afcf655
Wences81 <zhylinpochta@gmail.com>
2024-02-19 16:27:00 +0100

    19/02/2024

--- js/app.js
+++ js/app.js
@@ -6,20 +6,28 @@ function createNote(title, text) {
     noteEl.classList.add('note');
     noteEl.innerHTML = `
     <div class='note-header'>
-      <p>${title}</p>
+      <p id='note-title'>${title}</p>
+      <input id='note-title-input' class='hidden'>${title}</input>
       <div class='note-actions'>
         <button class='note-edit'><i class="fa-solid fa-pen-to-square"></i></button>
         <button class='note-delete'><i class="fa-solid fa-trash"></i></button>
       </div>
     </div>
-    <p id='note-description'>${text}</p>
+    <p id='note-text'>${text}</p>
+    <textarea id='note-textarea' class='hidden'>${text}</textarea>
     `
   
   const editBtn = noteEl.querySelector('.note-edit');
   const deleteBtn = noteEl.querySelector('.note-delete');
+  const titleEl = noteEl.querySelector('#note-title');
+  const textEl = noteEl.querySelector('#note-text');
+  const titleInputEl = noteEl.querySelector('#note-title-input');
+  const textInputEl = noteEl.querySelector('#note-textarea');
+
 
   editBtn.addEventListener('click', (e) => {
-
+    titleEl.classList.toggle('hidden');
+    textEl.classList.toggle('hidden');
   });
 
   deleteBtn.addEventListener('click', (e) => {
